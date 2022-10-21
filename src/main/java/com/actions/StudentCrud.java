22ad5673e81c2d6ca9839d02f0d03505c2037517
Vimal-Baskar <vimal.baskar@zohocorp.com>
2022-10-21 10:10:55 +0530

    Add status code

--- src/main/java/com/actions/StudentCrud.java
+++ src/main/java/com/actions/StudentCrud.java
@@ -1,6 +1,5 @@
 package com.actions;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,10 +28,16 @@ public class StudentCrud implements CrudHandler{
 		try 
 		{
 			student = (StudentPojo) JSONConvertor.convertJSONToPojo(json,student);
-//			StudentValidation.getValidation(student);
 			mapping = PojoConvertor.convertPojoToMap("student", student);
 			List<Map<String,Object>> dbResponse =  DB.getData("student",mapping);
 			JSONObject response = new JSONObject();
+			if(dbResponse.size()==0)
+			{
+				response.put("status_code",204);
+			}
+			else {
+				response.put("status_code", 200);
+			}
 			response.put("students", new JSONArray(dbResponse));
 			return response;
 		}
@@ -42,7 +47,7 @@ public class StudentCrud implements CrudHandler{
 			{
 				JSONObject fields = new JSONObject();
 				fields.put("fields",e.getMessage());
-				throw new GenericException("FIELD_NOT_FOUND_ERROR", "Not able to find the field", fields, "ERROR");
+				throw new GenericException("FIELD_NOT_FOUND_ERROR", "Not able to find the field", fields, "ERROR",400);
 			}
 			catch(GenericException except) 
 			{
@@ -62,7 +67,7 @@ public class StudentCrud implements CrudHandler{
 			e.printStackTrace();
 			try 
 			{
-				throw new GenericException("INTERNAL_SERVER_ERROR", "Something went wrong", new JSONObject(), "ERROR");
+				throw new GenericException("INTERNAL_SERVER_ERROR", "Something went wrong", new JSONObject(), "ERROR",500);
 			}
 			catch(GenericException except) 
 			{
@@ -83,12 +88,13 @@ public class StudentCrud implements CrudHandler{
 	public JSONObject delete(JSONObject json) 
 	{
 		try 
-		{
+		{			mapping.remove("student_id");
+
 			student = (StudentPojo) JSONConvertor.convertJSONToPojo(json,student);
 			JSONObject fields = StudentValidation.getValidation(student);
 			mapping = PojoConvertor.convertPojoToMap("student", student);
 			int dbResponse = DB.deleteData("student", mapping);
-			GenericException ge = new GenericException("DELETION_SUCCESS","student deleted successfully", fields, "SUCCESS");
+			GenericException ge = new GenericException("DELETION_SUCCESS","student deleted successfully", fields, "SUCCESS",200);
 			ge.setError(false);
 			return new JSONObject(ge.getMessage());
 		} 
@@ -108,7 +114,7 @@ public class StudentCrud implements CrudHandler{
 			e.printStackTrace();
 			try 
 			{
-				throw new GenericException("INTERNAL_SERVER_ERROR", "Something went wrong", new JSONObject(), "ERROR");
+				throw new GenericException("INTERNAL_SERVER_ERROR", "Something went wrong", new JSONObject(), "ERROR",500);
 			}
 			catch(GenericException except) 
 			{
@@ -131,14 +137,14 @@ public class StudentCrud implements CrudHandler{
 		try 
 		{
 			student = (StudentPojo) JSONConvertor.convertJSONToPojo(json,student);
-			student.setId(0);
 			GenericValidation.validate("studentCrud",student);
 			StudentValidation.insertionValidation(student);
 			JSONObject fields = new JSONObject();
 			mapping = PojoConvertor.convertPojoToMap("student", student);
+			mapping.remove("student_id");
 			int dbResponse = DB.insertData("student",mapping);
 			fields.put("id",dbResponse);
-			GenericException ge = new GenericException("INSERTION_SUCCESS","student inserted successfully", fields, "SUCCESS");
+			GenericException ge = new GenericException("INSERTION_SUCCESS","student inserted successfully", fields, "SUCCESS",200);
 			ge.setError(false);
 			return new JSONObject(ge.getMessage());
 		} 
@@ -157,7 +163,7 @@ public class StudentCrud implements CrudHandler{
 			e.printStackTrace();
 			try 
 			{
-				throw new GenericException("INTERNAL_SERVER_ERROR", "Something went wrong", new JSONObject(), "ERROR");
+				throw new GenericException("INTERNAL_SERVER_ERROR", "Something went wrong", new JSONObject(), "ERROR",500);
 			}
 			catch(GenericException except) 
 			{
@@ -178,16 +184,16 @@ public class StudentCrud implements CrudHandler{
 	{
 		try {
 			student = (StudentPojo) JSONConvertor.convertJSONToPojo(json.optJSONObject("data"),student);
-			student.setId(0);
 			StudentPojo student1 = new StudentPojo();
 			student1 = (StudentPojo) JSONConvertor.convertJSONToPojo(json.optJSONObject("where"),student1);
 			JSONObject fields = new JSONObject();
 			GenericValidation.validate("studentCrud",student);
 			fields = StudentValidation.updationValidation(student, student1);
 			mapping = PojoConvertor.convertPojoToMap("student", student);
+			mapping.remove("student_id");
 			Map<String, Object> whereTo = PojoConvertor.convertPojoToMap("student", student1);
 			int dbResponse = DB.updateData("student",mapping,whereTo);
-			GenericException ge = new GenericException("UPDATION_SUCCESS","students updated successfully", fields, "SUCCESS");
+			GenericException ge = new GenericException("UPDATION_SUCCESS","students updated successfully", fields, "SUCCESS",200);
 			ge.setError(false);
 			return new JSONObject(ge.getMessage());
 		}
@@ -203,7 +209,7 @@ public class StudentCrud implements CrudHandler{
 			e.printStackTrace();
 			try 
 			{
-				throw new GenericException("INTERNAL_SERVER_ERROR", "Something went wrong", new JSONObject(), "ERROR");
+				throw new GenericException("INTERNAL_SERVER_ERROR", "Something went wrong", new JSONObject(), "ERROR",500);
 			}
 			catch(GenericException except) 
 			{
